92106a66681d949388d2b7594159c4d7da9c1603
Leonardo Valadares <leonardo@debian.home>
2018-05-17 01:00:41 -0300

    lista prs criada

--- components/PullRequestList.vue
+++ components/PullRequestList.vue
@@ -1,15 +1,39 @@
 <template>
-    
+  <div v-if="prs !== null">
+    <ul>
+        <li v-for="pr in prs"> {{ pr.title }} {{ pr.user.login }}</li>
+    </ul>
+  </div>
+  <div v-else>
+    Loading...
+  </div>
 </template>
 
 <script>
+import axios from '~/plugins/axios'
+
+const fetchRepoPRs = async (repoId) => {
+  try {
+    const { data } = await axios.get(`/repositories/${repoId}/pulls`)
+    return data
+  } catch (error) {
+    return null
+  }
+}
+
 export default {
-    props: {
-        repoId: { type: String, required: true }
-    }
+  props: {
+    repoId: { type: Number, required: true }
+  },
+  data: () => {
+    return { prs: null }
+  },
+  async mounted () {
+    const fetchedPRs = await fetchRepoPRs(this.repoId)
+    this.prs = fetchedPRs
+  }
 }
 </script>
 
 <style>
-
 </style>
--- pages/_id.vue
+++ pages/_id.vue
@@ -2,11 +2,13 @@
   <section class="container">
     <h1> {{ repoInfo.full_name }} </h1>
     <h3> {{ repoInfo.description }} </h3>
+    <pull-request-list :repo-id="repoInfo.id"></pull-request-list>
   </section>
 </template>
 
 <script>
 import axios from '~/plugins/axios'
+import PullRequestList from '~/components/PullRequestList'
 
 const fetchRepoInfos = async (repoId) => {
   let { data } = await axios.get(`/repositories/${repoId}`)
@@ -15,6 +17,9 @@ const fetchRepoInfos = async (repoId) => {
 
 export default {
   name: 'id',
+  components: {
+    PullRequestList
+  },
   async asyncData ({ params, error }) {
     return { repoInfo: await fetchRepoInfos(params.id) }
     // error({ statusCode: 404, message: 'User not found' })
